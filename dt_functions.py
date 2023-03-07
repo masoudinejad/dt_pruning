08193f37bcc0faf300c14f43b337527ee775d6a0
Mojtaba Masoudinejad <mojtaba.masoudinejad@bosch.com>
2023-03-07 10:32:07 +0100

    cleaned leaves count function

--- dt_functions.py
+++ dt_functions.py
@@ -1,8 +1,2 @@
 from ExtendDT import ext_dt
 import numpy as np
-
-def leaves_count(base_dt):
-    X_dt = ext_dt(base_dt)
-    X_dt.findLeafs()
-    leaves_count = np.sum(X_dt.is_leaf)
-    return leaves_count
